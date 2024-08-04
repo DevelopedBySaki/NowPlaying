a9bdc5f84aa9c49d05b2513f60c3b96b766c7492
Svilen Grandzhan <new.tube.student@gmail.com>
2024-08-04 11:54:56 -0700

    main api request 20240804

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 import { secrets } from '../secrets/secrets.js';
 
 const fetchData = async (reference) => {
-  const response = await fetch(`${secrets.url}${reference}?api_key=${secrets.apiKey}&language=en-US&sort_by=vote_average.desc`);
+  const response = await fetch(`${secrets.url}${reference}?api_key=${secrets.apiKey}&language=en-US&with_original_language=en`);
+  // ToDo: check how exactly original language is working. It is showing movies on other languages
   const data = await response.json();
   return data;
 };
 
-const actors = await fetchData('person/popular');
+const movieNowPlaying = await fetchData('movie/now_playing');
+console.log(movieNowPlaying.results);
 
-console.log(actors.results[0]);
+const moviesPopular = await fetchData('movie/popular');
+console.log(moviesPopular.results);
+
+const showsNowPlaying = await fetchData('tv/on_the_air');
+console.log(showsNowPlaying.results);
+
+const showsPopular = await fetchData('tv/popular');
+console.log(showsPopular.results);
+
+const actorsPopular = await fetchData('person/popular');
+console.log(actorsPopular.results);
