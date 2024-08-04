9faac0ad031a7cd9af0f2f9d88c9af7efbc42b44
Svilen Grandzhan <peructep@gmail.com>
2024-08-04 08:36:21 -0700

    latest from 20240730

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("now playing web app");
+console.log("index.html page loaded");
+
+const ActorsDetailsGet = async () => {
+  const url = "https://api.themoviedb.org/3/person/person_id?language=en-US";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authentication: "None",
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error~);
+  }
+};
+
+ActorsDetailsGet();
